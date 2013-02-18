a11b24a6420a6038fa2a77d8588eee9be2bee798
Sitewards <github@sitewards.com>
2013-02-18 20:59:39 +0100

    Commit basic module files plus the widget chooser block

---
+++ app/code/community/Manners/Widgets/Block/Catalog/Product/Widget/Chooser.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Manners_Widgets_Block_Catalog_Product_Widget_Chooser
+ *
+ * @author      david.manners
+ * @category    Manners
+ * @package     Manners_Widgets
+ */
+class Manners_Widgets_Block_Catalog_Product_Widget_Chooser extends Mage_Adminhtml_Block_Catalog_Product_Widget_Chooser {
+	/**
+	 * Prepare chooser element HTML,
+	 *  - get the config variable 'use_massaction' from the xml,
+	 *    - if it is true then use this when creating the url
+	 *
+	 * @param Varien_Data_Form_Element_Abstract $oElement Form Element
+	 * @return Varien_Data_Form_Element_Abstract
+	 */
+	public function prepareElementHtml(Varien_Data_Form_Element_Abstract $oElement) {
+		$bUseMassAction = false;
+		if($this->getConfig('use_massaction') == TRUE) {
+			$bUseMassAction = true;
+		}
+		$iUniqueId = Mage::helper('core')->uniqHash($oElement->getId());
+		$aUrlVariables = array(
+			'uniq_id'			=> $iUniqueId,
+			'use_massaction'	=> $bUseMassAction
+		);
+		$sSourceUrl = $this->getUrl('*/catalog_product_widget/chooser', $aUrlVariables);
+
+		$oChooser = $this->getLayout()->createBlock('widget/adminhtml_widget_chooser')
+			->setElement($oElement)
+			->setTranslationHelper($this->getTranslationHelper())
+			->setConfig($this->getConfig())
+			->setFieldsetId($this->getFieldsetId())
+			->setSourceUrl($sSourceUrl)
+			->setUniqId($iUniqueId);
+
+		if ($oElement->getValue()) {
+			$aValue = explode('/', $oElement->getValue());
+			$iProductId = FALSE;
+			if (isset($aValue[0]) && isset($aValue[1]) && $aValue[0] == 'product') {
+				$iProductId = $aValue[1];
+			}
+			$iCategoryId = isset($aValue[2]) ? $aValue[2] : false;
+			$sLabel = '';
+			if ($iCategoryId) {
+				$sLabel = Mage::getResourceSingleton('catalog/category')
+					->getAttributeRawValue($iCategoryId, 'name', Mage::app()->getStore()) . '/';
+			}
+			if ($iProductId) {
+				$sLabel .= Mage::getResourceSingleton('catalog/product')
+					->getAttributeRawValue($iProductId, 'name', Mage::app()->getStore());
+			}
+			$oChooser->setLabel($sLabel);
+		}
+
+		$oElement->setData('after_element_html', $oChooser->toHtml());
+		return $oElement;
+	}
+}
---
+++ app/code/community/Manners/Widgets/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Manners_Widgets_Helper_Data
+ *
+ * Create an empty helper class to provide translations
+ *
+ * @author      david.manners
+ * @category    Manners
+ * @package     Manners_Widgets
+ */
+class Manners_Widgets_Helper_Data extends Mage_Core_Helper_Abstract {
+}
