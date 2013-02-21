5f67edcb4236f7d26a9495b8049c34367dc1e75c
David Manners <dmanners87@gmail.com>
2013-02-21 12:10:16 +0100

    rewrite adminhtml/catalog_product_widget block
    rewrite the block to add a check for massaction on the function
    getMainButtonsHtml

--- app/code/community/Manners/Widgets/Block/Catalog/Product/Widget/Chooser.php
+++ app/code/community/Manners/Widgets/Block/Catalog/Product/Widget/Chooser.php
@@ -8,6 +8,30 @@
  */
 class Manners_Widgets_Block_Catalog_Product_Widget_Chooser extends Mage_Adminhtml_Block_Catalog_Product_Widget_Chooser {
 	/**
+	 * Extend the getMainButtonsHtml function to check if we have a chooser using 'use_massaction'
+	 * @return string
+	 */
+	public function getMainButtonsHtml() {
+		$sHtml = '';
+		if($this->getFilterVisibility()){
+			$sHtml.= $this->getResetFilterButtonHtml();
+			$sHtml.= $this->getSearchButtonHtml();
+			if($this->hasMassaction()) {
+				$sHtml.= $this->getSearchButtonHtml();
+			}
+		}
+		return $sHtml;
+	}
+
+	/**
+	 * Return the attribute 'use_massaction'
+	 *
+	 * @return mixed
+	 */
+	public function hasMassaction() {
+		return $this->getData('use_massaction');
+	}
+	/**
 	 * Prepare chooser element HTML,
 	 *  - get the config variable 'use_massaction' from the xml,
 	 *    - if it is true then use this when creating the url
@@ -16,9 +40,9 @@ class Manners_Widgets_Block_Catalog_Product_Widget_Chooser extends Mage_Adminhtm
 	 * @return Varien_Data_Form_Element_Abstract
 	 */
 	public function prepareElementHtml(Varien_Data_Form_Element_Abstract $oElement) {
-		$bUseMassAction = false;
-		if($this->getConfig('use_massaction') == true) {
-			$bUseMassAction = true;
+		$bUseMassAction = FALSE;
+		if($this->getConfig('use_massaction') == TRUE) {
+			$bUseMassAction = TRUE;
 		}
 		$iUniqueId = Mage::helper('core')->uniqHash($oElement->getId());
 		$aUrlVariables = array(
@@ -41,7 +65,7 @@ class Manners_Widgets_Block_Catalog_Product_Widget_Chooser extends Mage_Adminhtm
 			if (isset($aValue[0]) && isset($aValue[1]) && $aValue[0] == 'product') {
 				$iProductId = $aValue[1];
 			}
-			$iCategoryId = isset($aValue[2]) ? $aValue[2] : false;
+			$iCategoryId = isset($aValue[2]) ? $aValue[2] : FALSE;
 			$sLabel = '';
 			if ($iCategoryId) {
 				$sLabel = Mage::getResourceSingleton('catalog/category')
