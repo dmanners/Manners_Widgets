7f097e915070e5470ae664d148e513403d322498
David Manners <dmanners87@gmail.com>
2013-03-21 17:06:13 +0100

    Use the _prepareMassaction function
    Just use the massaction information from the product at the moment.

--- app/code/community/Manners/Widgets/Block/Catalog/Product/Widget/Chooser.php
+++ app/code/community/Manners/Widgets/Block/Catalog/Product/Widget/Chooser.php
@@ -8,77 +8,68 @@
  */
 class Manners_Widgets_Block_Catalog_Product_Widget_Chooser extends Mage_Adminhtml_Block_Catalog_Product_Widget_Chooser {
 	/**
-	 * Extend the getMainButtonsHtml function to check if we have a chooser using 'use_massaction'
-	 * @return string
+	 *
+	 * @return Mage_Adminhtml_Block_Widget_Grid|void
 	 */
-	public function getMainButtonsHtml() {
-		$sHtml = '';
-		if($this->getFilterVisibility()){
-			$sHtml.= $this->getResetFilterButtonHtml();
-			$sHtml.= $this->getSearchButtonHtml();
-			if($this->hasMassaction()) {
-				$sHtml.= $this->getSearchButtonHtml();
-			}
-		}
-		return $sHtml;
+	protected function _prepareColumns() {
+		$this->addColumn('entity_id', array(
+			'header'    => Mage::helper('catalog')->__('ID'),
+			'sortable'  => true,
+			'width'     => '60px',
+			'index'     => 'entity_id'
+		));
+		$this->addColumn('chooser_sku', array(
+			'header'    => Mage::helper('catalog')->__('SKU'),
+			'name'      => 'chooser_sku',
+			'width'     => '80px',
+			'index'     => 'sku'
+		));
+		$this->addColumn('chooser_name', array(
+			'header'    => Mage::helper('catalog')->__('Product Name'),
+			'name'      => 'chooser_name',
+			'index'     => 'name'
+		));
 	}
 
 	/**
-	 * Return the attribute 'use_massaction'
 	 *
-	 * @return mixed
+	 * @return $this|Mage_Adminhtml_Block_Widget_Grid
 	 */
-	public function hasMassaction() {
-		return $this->getData('use_massaction');
-	}
-	/**
-	 * Prepare chooser element HTML,
-	 *  - get the config variable 'use_massaction' from the xml,
-	 *    - if it is true then use this when creating the url
-	 *
-	 * @param Varien_Data_Form_Element_Abstract $oElement Form Element
-	 * @return Varien_Data_Form_Element_Abstract
-	 */
-	public function prepareElementHtml(Varien_Data_Form_Element_Abstract $oElement) {
-		$bUseMassAction = FALSE;
-		if($this->getConfig('use_massaction') == TRUE) {
-			$bUseMassAction = TRUE;
-		}
-		$iUniqueId = Mage::helper('core')->uniqHash($oElement->getId());
-		$aUrlVariables = array(
-			'uniq_id'			=> $iUniqueId,
-			'use_massaction'	=> $bUseMassAction
-		);
-		$sSourceUrl = $this->getUrl('*/catalog_product_widget/chooser', $aUrlVariables);
+	protected function _prepareMassaction() {
+		$this->setMassactionIdField('entity_id');
+		$this->getMassactionBlock()->setFormFieldName('product');
 
-		$oChooser = $this->getLayout()->createBlock('widget/adminhtml_widget_chooser')
-			->setElement($oElement)
-			->setTranslationHelper($this->getTranslationHelper())
-			->setConfig($this->getConfig())
-			->setFieldsetId($this->getFieldsetId())
-			->setSourceUrl($sSourceUrl)
-			->setUniqId($iUniqueId);
+		$this->getMassactionBlock()->addItem('delete', array(
+			'label'=> Mage::helper('catalog')->__('Delete'),
+			'url'  => $this->getUrl('*/*/massDelete'),
+			'confirm' => Mage::helper('catalog')->__('Are you sure?')
+		));
 
-		if ($oElement->getValue()) {
-			$aValue = explode('/', $oElement->getValue());
-			$iProductId = FALSE;
-			if (isset($aValue[0]) && isset($aValue[1]) && $aValue[0] == 'product') {
-				$iProductId = $aValue[1];
-			}
-			$iCategoryId = isset($aValue[2]) ? $aValue[2] : FALSE;
-			$sLabel = '';
-			if ($iCategoryId) {
-				$sLabel = Mage::getResourceSingleton('catalog/category')
-					->getAttributeRawValue($iCategoryId, 'name', Mage::app()->getStore()) . '/';
-			}
-			if ($iProductId) {
-				$sLabel .= Mage::getResourceSingleton('catalog/product')
-					->getAttributeRawValue($iProductId, 'name', Mage::app()->getStore());
-			}
-			$oChooser->setLabel($sLabel);
+		$statuses = Mage::getSingleton('catalog/product_status')->getOptionArray();
+
+		array_unshift($statuses, array('label'=>'', 'value'=>''));
+		$this->getMassactionBlock()->addItem('status', array(
+			'label'=> Mage::helper('catalog')->__('Change status'),
+			'url'  => $this->getUrl('*/*/massStatus', array('_current'=>true)),
+			'additional' => array(
+				'visibility' => array(
+					'name' => 'status',
+					'type' => 'select',
+					'class' => 'required-entry',
+					'label' => Mage::helper('catalog')->__('Status'),
+					'values' => $statuses
+				)
+			)
+		));
+
+		if (Mage::getSingleton('admin/session')->isAllowed('catalog/update_attributes')){
+			$this->getMassactionBlock()->addItem('attributes', array(
+				'label' => Mage::helper('catalog')->__('Update Attributes'),
+				'url'   => $this->getUrl('*/catalog_product_action_attribute/edit', array('_current'=>true))
+			));
 		}
 
-		$oElement->setData('after_element_html', $oChooser->toHtml());
-		return $oElement;
+		Mage::dispatchEvent('adminhtml_catalog_product_grid_prepare_massaction', array('block' => $this));
+		return $this;
 	}
 }
