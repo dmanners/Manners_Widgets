7d4124ae318e25ccd0a1398c2cf1ff17f71d20bf
Erick Patrick <contato@erickpatrick.net>
2016-05-02 23:41:38 +0000

    Add rendering block and template for widget

--- app/code/community/Manners/Widgets/Model/Collection/Product.php
+++ app/code/community/Manners/Widgets/Model/Collection/Product.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @category    Manners
- * @package     Manners_Widgets
- * @copyright   Copyright (c) David Manners (http://davidmanners.de/)
- */
-
 class Manners_Widgets_Model_Collection_Product
 {
     private $oProductCollection;
@@ -30,17 +24,15 @@ class Manners_Widgets_Model_Collection_Product
         $aProductIds = array_map('intval', explode(',', $sProductIds));
 
         $this->addAttributesToCollection();
-        $this->addFiltersToCollection($aProductIds);
-        $this->addSortingToCollection($aProductIds);
+
+        $this->oProductCollection->addFieldToFilter('entity_id', array('in' => $aProductIds));
+
+        $sCollectionOrder = new Zend_Db_Expr('FIELD(e.entity_id, ' . implode(',', $aProductIds).')');
+        $this->oProductCollection->getSelect()->order($sCollectionOrder);
 
         return $this->oProductCollection;
     }
 
-    /**
-     * Add basic product attributes to collection
-     *
-     * @return $this
-     */
     private function addAttributesToCollection()
     {
         $this->oProductCollection->addAttributeToSelect(
@@ -49,41 +41,6 @@ class Manners_Widgets_Model_Collection_Product
         $this->oProductCollection->addMinimalPrice();
         $this->oProductCollection->addFinalPrice();
         $this->oProductCollection->addTaxPercents();
-        $this->oProductCollection->addWebsiteNamesToResult();
-        $this->oProductCollection->addTierPriceData();
-
-        return $this;
-    }
-
-    /**
-     * Add various filters to collection
-     *
-     * @param $aProductIds
-     *
-     * @return $this
-     */
-    private function addFiltersToCollection($aProductIds)
-    {
-        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($this->oProductCollection);
-        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($this->oProductCollection);
-        $this->oProductCollection->addIdFilter($aProductIds);
-        $this->oProductCollection->addStoreFilter();
-        $this->oProductCollection->addWebsiteFilter();
-
-        return $this;
-    }
-
-    /**
-     * Add sorting through entity_id to collection
-     *
-     * @param $aProductIds
-     *
-     * @return $this
-     */
-    private function addSortingToCollection($aProductIds)
-    {
-        $sCollectionOrder = new Zend_Db_Expr('FIELD(entity_id, ' . implode(',', $aProductIds) . ')');
-        $this->oProductCollection->getSelect()->order($sCollectionOrder);
 
         return $this;
     }
